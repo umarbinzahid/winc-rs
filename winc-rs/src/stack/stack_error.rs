032c82f4e7694badbd359f2df54af9848df9c444
umarbinzahid <142439476+umarbinzahid@users.noreply.github.com>
2025-08-31 16:46:58 +0500

    Added comments in stack_error.rs

--- winc-rs/src/stack/stack_error.rs
+++ winc-rs/src/stack/stack_error.rs
@@ -10,82 +10,106 @@ use embedded_nal::nb;
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug, PartialEq)]
 pub enum StackError {
+    /// This operation requires blocking behavior to complete.
     WouldBlock,
+    /// Operation timed out.
     GeneralTimeout,
-    /// TCP connection timed out
+    /// TCP/UDP connection timed out.
     ConnectTimeout,
+    /// Receiving data from socket timed out.
     RecvTimeout,
+    /// Sending data to socket timed out.
     SendTimeout,
+    /// No more sockets can be configured.
     OutOfSockets,
+    /// Socket is already in use.
     SocketAlreadyInUse,
+    /// Closing socket failed.
     CloseFailed,
+    /// Unexpected error occurred.
     Unexpected,
+    /// Error occurred while processing event.
     DispatchError(crate::errors::CommError),
+    /// Sending connect socket request failed.
     ConnectSendFailed(crate::errors::CommError),
+    /// Receiving data from socket failed.
     ReceiveFailed(crate::errors::CommError),
+    /// Sending send request failed.
     SendSendFailed(crate::errors::CommError),
+    /// Sending close request failed.
     SendCloseFailed(crate::errors::CommError),
+    /// Binding socket failed.
     BindFailed(crate::errors::CommError),
+    /// Error occurred while communicating with WINC module.
     WincWifiFail(crate::errors::CommError),
+    /// Socket operation failed.
     OpFailed(SocketError),
-    /// DNS lookup timed out
+    /// DNS lookup timed out.
     DnsTimeout,
-    /// Unexpected DNS error
+    /// Unexpected DNS error.
     DnsFailed,
-    /// Operation was attempted in wrong state
+    /// Operation was attempted in an invalid state.
     InvalidState,
+    /// Module is already connected to an access point.
     AlreadyConnected,
-    /// Acess point join failed
+    /// Access point join failed.
     ApJoinFailed(WifiConnError),
-    /// Scan operation failed
+    /// Scan operation failed.
     ApScanFailed(WifiConnError),
-    // Continue
+    /// Continue the operation.
     ContinueOperation,
-    /// Not found
+    /// Socket not found.
     SocketNotFound,
-    /// Parameters are not valid.
+    /// Invalid parameters.
     InvalidParameters,
     #[cfg(feature = "experimental-ota")]
-    /// Ota Error
+    /// OTA error.
     OtaFail(OtaUpdateError),
 }
 
+/// Converts `core::convert::Infallible` to `StackError`.
 impl From<core::convert::Infallible> for StackError {
     fn from(_: core::convert::Infallible) -> Self {
         unreachable!()
     }
 }
 
+/// Converts a `SocketError` into a `StackError` by wrapping it in `OpFailed`.
 impl From<SocketError> for StackError {
     fn from(inner: SocketError) -> Self {
         Self::OpFailed(inner)
     }
 }
 
+/// Converts a `CommError` into a `StackError` as a `WincWifiFail` variant.
 impl From<crate::errors::CommError> for StackError {
     fn from(inner: crate::errors::CommError) -> Self {
         Self::WincWifiFail(inner)
     }
 }
 
+/// Converts a `CommError` directly into an `nb::Error<StackError>` for use with non-blocking APIs.
 impl From<crate::errors::CommError> for nb::Error<StackError> {
     fn from(inner: crate::errors::CommError) -> Self {
         nb::Error::Other(StackError::WincWifiFail(inner))
     }
 }
 
+/// Converts a `core::net::AddrParseError` into a `StackError::InvalidParameters`,
 impl From<core::net::AddrParseError> for StackError {
     fn from(_: core::net::AddrParseError) -> Self {
         Self::InvalidParameters
     }
 }
 
+/// Implements the `TcpError` trait from `embedded-nal` for `StackError`,
 impl embedded_nal::TcpError for StackError {
     fn kind(&self) -> embedded_nal::TcpErrorKind {
         embedded_nal::TcpErrorKind::Other
     }
 }
 
+/// Converts a non-blocking error (`nb::Error<StackError>`) back into a `StackError`.
 impl From<nb::Error<StackError>> for StackError {
     fn from(inner: nb::Error<StackError>) -> Self {
         match inner {
@@ -95,6 +119,7 @@ impl From<nb::Error<StackError>> for StackError {
     }
 }
 
+/// Implements the `Display` trait for `StackError`
 impl core::fmt::Display for StackError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
